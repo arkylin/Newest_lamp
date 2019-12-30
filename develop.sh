bdca5b9d4a088b95e3f86e33b97ae2cdd71311b7
旋风凌 <x@xyz.blue>
2019-12-30 21:38:17 +0800

    Update develop.sh

--- develop.sh
+++ develop.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+Other_files_for_lamp="https://raw.githubusercontent.com/arkylin/other_files_for_lamp/master"
 pkgList="diffutils nghttp2 libnghttp2 libnghttp2-devel java jemalloc jemalloc-devel openssh-server python python-devel python2 python2-devel oniguruma-devel rpcgen go htop libicu icu deltarpm gcc gcc-c++ make cmake autoconf libjpeg libjpeg-devel libjpeg-turbo libjpeg-turbo-devel libpng libpng-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel krb5-devel libc-client libc-client-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel libaio numactl numactl-libs readline-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5-devel libidn libidn-devel openssl openssl-devel libxslt-devel libicu-devel libevent-devel libtool libtool-ltdl bison gd-devel vim-enhanced pcre pcre-devel libmcrypt libmcrypt-devel mhash mhash-devel mcrypt zip unzip sqlite-devel sysstat patch bc expect expat-devel oniguruma oniguruma-devel libtirpc-devel nss nss-devel rsync rsyslog git lsof lrzsz psmisc wget which libatomic tmux"
 for Package in ${pkgList}; do
     dnf -y install ${Package}
@@ -9,7 +10,7 @@ sed -i "s@^#PermitRootLogin.*@PermitRootLogin yes@" /etc/ssh/sshd_config
 systemctl enable sshd
 
 cd /develop
-wget https://raw.githubusercontent.com/arkylin/open_shell/master/options.conf
+wget ${Other_files_for_lamp}/options.conf
 
 . /develop/options.conf
 
@@ -25,7 +26,7 @@ if [ -e "${source_dir}/apr-${apr_version}.tar.gz" ]; then
     tar xzf ${source_dir}/apr-${apr_version}.tar.gz
     cd ${source_dir}/apr-${apr_version}
     rm -rf ${source_dir}/apr-${apr_version}/configure
-    wget https://raw.githubusercontent.com/arkylin/open_shell/master/apr-${apr_version}/configure
+    wget ${Other_files_for_lamp}/apr-${apr_version}/configure
     chmod +x configure
     ./configure --prefix=${apr_install_dir}
     make -j ${THREAD} && make install
@@ -70,7 +71,7 @@ if [ -e "${apr_install_dir}/bin/apu-1-config" ]; then
 #结束安装 apr-util
 
 # 安装 Apache
-wget http://mirrors.tuna.tsinghua.edu.cn/apache//httpd/httpd-${Apache_version}.tar.gz
+wget ${Mirror_source}/apache//httpd/httpd-${Apache_version}.tar.gz
 if [ -e "${source_dir}/httpd-${Apache_version}.tar.gz" ]; then
     echo "Apr download successfully! "
     tar xzf ${source_dir}/httpd-${Apache_version}.tar.gz
@@ -82,7 +83,7 @@ if [ -e "${source_dir}/httpd-${Apache_version}.tar.gz" ]; then
   else
     echo "Apache download Failed! "
   fi
-if [ -e "${apr_install_dir}/bin/apu-1-config" ]; then
+if [ -e "${apache_install_dir}/bin/httpd" ]; then
     echo "Apache installed successfully! "
     echo "Apache installed successfully! "
     echo "Apache installed successfully! "
